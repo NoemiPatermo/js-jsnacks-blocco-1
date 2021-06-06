c90b54540ba49939e174f0e9e0d7a86b091cae41
noemi86-cpu <noemipatermo@gmail.com>
2021-06-06 22:47:58 +0200

    js-snack

--- js-snack/js-snack-8/js/script.js
+++ js-snack/js-snack-8/js/script.js
@@ -2,21 +2,39 @@
 //e calcola la somma di tutte le cifre che compongono il numero.
 //(bonus: controllate che il numero sia realmente di 4 cifre altrimenti restituite un alert)
 
-var askNumber = prompt ("inserire un numero di 4 cifre");
+var userNumber = prompt ("inserire un numero di 4 cifre");
 var sum = 0;
-if (askNumber.length !==4){
+if (userNumber.length !==4){
     alert ("Attento! Il numero deve essere di 4 cifre!")
-}else if(isNaN(askNumber)){
+}else if(isNaN(userNumber)){
     alert ("Inserisci un numero")
 
 }else{
-    for(i = 0; i < askNumber.length; i++){
-        sum += parseInt(askNumber[i]);
+    for(i = 0; i < userNumber.length; i++){
+        sum += parseInt(userNumber[i]);
     }
     document.getElementById('result').innerHTML += sum;
 } 
 
+/*CORREZIONE CON OTTAVIO
+var userNumber = prompt ("inserisci un numero");
+inputLen = userNumber.length;
 
+if(inputLen !== 4){
+    alert("Ehi, devi inserire un numero di 4 cifre!")
+}else if(isNaN(userNumber)){
+    alert(Attento! Inserisci un numero!)
+}else{
+    var result = 0;
+    for(var i = 0; i < inputLen; i++){
+    result += pareseInt(userNumber[i]);
+}
+    console.log(result);
+}
+
+NaN ti sta dicendo che non è un numero anche se prova a fare ugualmete le operazione (in quesro caso aver inserito 
+    una parola di 4 lettere)
+-------------------------------------------------------------------------------
 //continua a chiedere la cifra col prompt finchè non è di 4 cifre
 /* avvia il while
 
